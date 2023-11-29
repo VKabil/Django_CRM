d9e944402de14beaae4d289ab2e4863c876ff334
VKabil <kabilvkabil@gmail.com>
2023-11-29 20:36:04 +0530

    some pages added in crm_website app

--- crm_website/views.py
+++ crm_website/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+        if request.method == 'POST':
+              username = request.POST['username']
+              password = request.POST['password']
+              user = authenticate(request, username=username, password=password)
+              if user is not None:
+                    login(request, user)
+                    messages.success(request, "You have been successfully Logged in......")
+                    return redirect('home')
+              else:
+                    messages.success(request, "Your username or password incorrect, Please check the field")
+                    return redirect('home')
+        else:      
+            return render(request, 'home.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been successfully logged out.....")
+    return redirect('home')
+
+
