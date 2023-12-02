62a212c9dee8b841cc22101bf97324e299fd186f
VKabil <kabilvkabil@gmail.com>
2023-12-02 11:09:41 +0530

    add display the list in web page

--- crm_website/models.py
+++ crm_website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    Email = models.CharField(max_length=50)
+    Phone_number = models.CharField(max_length=50)
+    Address = models.CharField(max_length=50)
+    State = models.CharField(max_length=50)
+    City = models.CharField(max_length=50)
+    Pin_code = models.CharField(max_length=50)
+
+    def __str__(self):
+        return(f"{self.first_name} {self.last_name}")
